6ac7312e3e79233d7f59cda2c0df01bde16d321c
JoshBenPhilip <joshben4c@gmail.com>
2022-10-08 13:38:47 -0400

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { testRoute } from "./API/index";
+import DashBoard from "./DashBoard";
+import HeatMap from "./HeatMap";
 
 function App() {
   testRoute((msg) => {
     console.log("API MESSAGE", msg);
   });
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <>
+      <div className="App">Hello world</div>
+      <h1>hi</h1>
+      <h2>This is the Upload Place Holder</h2>
+      <DashBoard />
+      <HeatMap />
+    </>
   );
 }
 
---
+++ client/src/HeatMap.jsx
@@ -0,0 +1,3 @@
+export default function HeatMap() {
+  return <h2>This is the HeatMap</h2>;
+}

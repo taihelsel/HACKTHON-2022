93678ee1b8351d18bd001914cc0120aade58051e
JoshBenPhilip <joshben4c@gmail.com>
2022-10-09 08:58:34 -0400

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,15 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <h1>Ticket N Fixit!</h1>
         <Routes>
           <Route path="/" element={<CreateTicketPage />} />
           <Route path="/heatmap" element={<HeatMapPage />} />
         </Routes>
+      </div>
+      <div className="bottom-nav">
+        <div className="button">Complain</div>
+        <div className="button">HeatMap</div>
       </div>
     </BrowserRouter>
   );

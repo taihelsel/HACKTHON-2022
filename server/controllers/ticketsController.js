1640d31672b6258c58477eaec3328cd11c4dd370
taihelsel <tai.bayard@gmail.com>
2022-10-09 06:36:46 -0400

    Conncted  ticket fetching and adding to firebase

--- server/controllers/ticketsController.js
+++ server/controllers/ticketsController.js
@@ -1,22 +1,19 @@
+const admin = require("firebase-admin"), db = admin.firestore();
+
 /* eslint-disable no-unused-vars */
-module.exports.all = (req, res) => {
+module.exports.all = async (req, res) => {
     // @route  GET /api/tickets/all/
     // @desc   Returns all tickets
     // @access Public
     try {
-        const data = [{
-            image: "https://via.placeholder.com/350x150",
-            desc: "Very real, very long desc",
-            location: "777 Glades Rd, Boca Raton, FL 33431",
-            score: 5190,
-        },
-        {
-            image: "https://via.placeholder.com/350x150",
-            desc: "Hmm. Very Real, yes",
-            location: "777 Glades Rd, Boca Raton, FL 33431",
-            score: 1095,
-        }
-        ]
+        const tickets = await db.collection("Ticket").get();
+        const data = [];
+        tickets.forEach(doc => {
+            data.push({
+                id:doc.id,
+                ...doc.data(),
+            })
+        });
         return res.status(200).json({ data, error: false });
     } catch (err) {
         console.log(err);
@@ -93,7 +90,7 @@ module.exports.remove = (req, res) => {
 }
 
 
-module.exports.add = (req, res) => {
+module.exports.add = async (req, res) => {
     // @route  POST /api/tickets/add/
     // @desc   Adds ticket to db
     // @access Public
@@ -101,11 +98,17 @@ module.exports.add = (req, res) => {
         const {
             imageURL,
             desc,
-            location,
+            coords,
         } = req.body;
-        //do create ticket logic here
-        console.log("Create ticket");
-        return res.status(200).json({ error: false, msg: "Ticket created successful" });
+        const tickets =  db.collection("Ticket");
+        await tickets.doc().set({
+            descriptions: desc,
+            location: new admin.firestore.GeoPoint(coords.latitude, coords.longitude),
+            picture:imageURL,
+            upvotes: Math.floor(Math.random() * 1500),
+            reports:0,
+        });
+        return res.status(200).json({ error: false, msg: "Ticket created successfully" });
     } catch (err) {
         console.log(err);
         return res.status(400).json({ error: { msg: "Error message" } });

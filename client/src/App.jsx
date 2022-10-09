97bf6d8408820c845b8212cafdf51105f08a38fd
JoshBenPhilip <joshben4c@gmail.com>
2022-10-09 08:28:45 -0400

    working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Navbar Placeholder</h1>
         <Routes>
           <Route path="/" element={<CreateTicketPage />} />
           <Route path="/heatmap" element={<HeatMapPage />} />

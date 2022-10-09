288451f61bfbec906d1a8dfc1f3607ea7e369a23
taihelsel <tai.bayard@gmail.com>
2022-10-08 22:08:52 -0400

    Added image capture button

--- client/src/pages/CreateTicketPage/components/UploadImage.jsx
+++ client/src/pages/CreateTicketPage/components/UploadImage.jsx
@@ -1,5 +1,49 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+import { useRef, useState } from "react";
+import PropTypes from "prop-types";
+
+function WebCam({ handleFail }) {
+  const video = useRef(null);
+  const canvas = useRef(null);
+
+  const startCameraClick = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    }).catch((err) => {
+      handleFail(true);
+    });
+    video.current.srcObject = stream;
+  };
+  const takePhotoClick = async () => {
+    const currentCanvas = canvas.current;
+    currentCanvas.getContext("2d").drawImage(video, 0, 0, currentCanvas.width, currentCanvas.height);
+    const imgDataURL = currentCanvas.toDataURL("image/jpeg");
+  };
+  return (
+    <>
+      <button type="button" id="open-camera-btn" onClick={startCameraClick}>Start Camera</button>
+      <video id="video" width="320" height="240" autoPlay ref={video} />
+      <button type="button" id="take-photo-btn" onClick={takePhotoClick}>Click Photo</button>
+      <canvas id="canvas" width="320" height="240" ref={canvas} />
+    </>
+  );
+}
+WebCam.propTypes = {
+  handleFail: PropTypes.func.isRequired,
+};
+function AccessFilesPrompt() {
+  return (
+    <input
+      type="file"
+      accept="image/png,image/jpg,image/jpeg,image/webp,capture=camera"
+      onChange={(e) => { console.log("change", e); }}
+      onInput={(e) => { console.log("input", e); }}
+    />
+  );
+}
 export default function UploadImage() {
   return (
-    <input type="file" accept="image/*;capture=camera" />
+    <AccessFilesPrompt />
   );
 }
